803bf759ef7c00bb4dadc38eec74d19d3e3e1385
Demetrio93 <jv-demetrio@hotmail.com>
2023-06-27 07:39:35 -0400

    Quinto commit

--- app/controllers/moedas_controller.rb
+++ app/controllers/moedas_controller.rb
@@ -1,5 +1,5 @@
 class MoedasController < ApplicationController
-  layout "adm"
+  # layout "adm"
   before_action :set_moeda, only: %i[ show edit update destroy ]
   before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
